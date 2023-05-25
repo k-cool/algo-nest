4f5bd93848da79500688238f0ff33e2044ebef02
GGAE99 <duddn1529@naver.com>
2023-05-25 13:11:57 +0900

    -commit tset

--- src/tests/tests.entity.ts
+++ src/tests/tests.entity.ts
@@ -1,5 +1,16 @@
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+/* @Entity({
+    name: 'TEST',
+    engine: 'MyISAM',
+    database: 'example_dev',
+    schema: 'schema_with_best_tables',
+    synchronize: false,
+    orderBy: {
+      name: 'ASC',
+      id: 'DESC'
+    }
+  }) */
 @Entity()
 export class Test extends BaseEntity {
     //auto increment
